bd3ae11a23f5ae235c8e802b9d291dcfb319e10d
Fanny Mozes <fanny.mozes@gmail.com>
2026-09-18 12:13:37 +0200

    setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vue 3 + TypeScript + Vite
+
+Zum starten des Projekts den Ordnerpfad und geben sie im Teminal
+
+`npm run`
+
+Press the Button um hochzuzählen oder runter zuzählen
+
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const count = ref(0)
+const doubled = computed(() => count.value *2)
+</script>
+
+<template>
+  <section id="center">
+    <span>Count is {{ count }}</span>
+    <button type="button" class="counter" @click="count++">
+      +1
+    </button>
+    <button type="button" class="counter" @click="count--">
+      -1
+    </button>
+    <span>The double of your count is {{ doubled }}</span>
+  </section>
+</template>
